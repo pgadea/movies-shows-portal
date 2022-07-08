ecbf73c269db135b71006ba50334a74c6328848f
Pierre Gadea <pgadea7@gmail.com>
2022-07-08 17:53:27 -0500

    changed app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,17 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
-import { SliderComponent } from './components/slider/slider.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SliderComponent } from './components/slider/slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 import { ItemComponent } from './components/item/item.component';
 import { PaginatorModule } from 'primeng/paginator';
+import { TabViewModule } from 'primeng/tabview';
 import { MovieComponent } from './pages/movie/movie.component';
-import {TabViewModule} from 'primeng/tabview';
 import { VideoEmbedComponent } from './components/video-embed/video-embed.component';
-import {ImageModule} from 'primeng/image';
-import {CarouselModule} from 'primeng/carousel';
+import { ImageModule } from 'primeng/image';
+import { CarouselModule } from 'primeng/carousel';
 import { GenresComponent } from './pages/genres/genres.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { TvShowsComponent } from './pages/tv-shows/tv-shows.component';
@@ -34,10 +34,10 @@ import { TvShowComponent } from './pages/tvshow/tvshow.component';
     ItemsBannerComponent,
     ItemComponent,
     MovieComponent,
-    GenresComponent,
-    TvShowComponent,
-    TvShowsComponent,
     VideoEmbedComponent,
+    GenresComponent,
+    TvShowsComponent,
+    TvShowComponent
   ],
   imports: [
     BrowserModule,
@@ -53,4 +53,4 @@ import { TvShowComponent } from './pages/tvshow/tvshow.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
